717d6645b1a5c389a29433003f02681833dc0630
hadleyhzy34 <hadley_hzy@hotmail.com>
2021-04-08 15:46:04 +0800

    dqn update

---
+++ dqn/dqn_torch/gym_test.py
@@ -0,0 +1,16 @@
+import gym
+import time
+
+env = gym.make('CartPole-v1')
+observation = env.reset() #position of cart,velocity of cart, angle of pole, rotation rate of pole
+
+for _ in range(1000):
+    env.render()
+    time.sleep(0.1)
+    #import ipdb; ipdb.set_trace()
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+
+    if done:
+        observation = env.reset()
+env.close()
