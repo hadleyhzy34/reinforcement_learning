63de26bd09cfad8b02ad289b6ba41ad605eb80cf
hadleyhzy34 <hadley_hzy@hotmail.com>
2021-08-04 21:57:30 +0800

    update mc

--- TD/monte_carlo.py
+++ TD/monte_carlo.py
@@ -20,9 +20,13 @@ def get_probs(q, env, epsilon):
     '''
     get the probability of taking the best known action according to epsion
     '''
+    # import ipdb;ipdb.set_trace()
     actions = np.argmax(q,axis=1)
-    policy_s = np.ones((env.observation_space.n, env.action_space.n))*epsilon/env.action_space.n
-    policy_s[:,actions] = 1 - epsilon + (epsilon / env.action_space.n)
+    policy_s = np.ones((env.observation_space.n, env.action_space.n))/env.action_space.n
+    for i in range(env.observation_space.n):
+        if q[i].any()!=0:
+            policy_s[i] = policy_s[i] * epsilon
+            policy_s[i,actions[i]] = 1 - epsilon + (epsilon / env.action_space.n)
     return policy_s
 
 def update_Q(env, episodes, Q, q):
@@ -33,27 +37,35 @@ def update_Q(env, episodes, Q, q):
         avg = sum/len(Q[(s,a)])
         q[s,a] = avg
 
-def run(env, Q, q, epsilon, gamma,r):
+def run(env, Q, q, epsilon, gamma):
     episodes = []
     state = env.reset()
     G = 0
+    r = 0
+    # import ipdb;ipdb.set_trace()
+    probs = get_probs(q, env, epsilon)
     while True:# loop for each step of episode
-        probs = get_probs(q, env, epsilon) # get the current behavior policy
+        # import ipdb;ipdb.set_trace()
+        # probs = get_probs(q, env, epsilon) # get the current behavior policy
         action = np.random.choice(np.arange(env.action_space.n),p=probs[state])\
-            if state in Q else env.action_space.sample()
+                if True in q[state]!=0 else env.action_space.sample()
         
         next_state, reward, done, _ = env.step(action)
         episodes.append((state, action, reward))
 
         G = gamma * G + reward
-        Q[(state, action)].append(G)
+        if (state,action) in Q:
+            Q[(state, action)].append(G)
+        else:
+            Q[(state,action)] = [G]
 
         r += reward
+        # print(f'current status:r:{r},state:{state},action:{action}')
 
         state = next_state
         if done:
             break
-    return episodes
+    return episodes,r
 
 # initialize q table
 q = np.zeros((env.observation_space.n, env.action_space.n))
@@ -63,12 +75,11 @@ Q = {}
 q_reward_record = []
 q_error_record = []
 
-import ipdb;ipdb.set_trace()
+# import ipdb;ipdb.set_trace()
 # loop for each episode:
 for episode in range(5000):
-    r = 0
     state = env.reset()
-    episodes = run(env,Q,q,epsilon,gamma,r)
+    episodes,r = run(env,Q,q,epsilon,gamma)
     update_Q(env,episodes,Q,q)
     
     if episode%100 == 0:
